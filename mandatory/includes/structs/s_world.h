f3b8e8e33070a4f76e6869a8f9adc1f05ac1f16f
yonaCluster <yonaimyo@gmail.com>
2023-02-23 05:25:10 +0900

    Change: s_object 배열 -> s_object 리스트 사용

--- mandatory/includes/structs/s_world.h
+++ mandatory/includes/structs/s_world.h
@@ -3,16 +3,17 @@
 
 # include "s_object.h"
 
-typedef struct s_object_arr
-{
-	t_object		*data;
-	size_t			capacity;
-	size_t			count;
-}	t_object_arr;
+// typedef struct s_object_arr
+// {
+// 	t_object		*data;
+// 	size_t			capacity;
+// 	size_t			count;
+// }	t_object_arr;
 
 typedef struct s_world
 {
-	t_object_arr	*objects;
+	// t_object_arr	*objects;
+	t_list			*object_list;
 	t_color			background_color;
 }	t_world;
 
