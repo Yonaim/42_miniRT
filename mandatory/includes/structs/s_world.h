91267b6efa3cd2fc5f894a58cb181dbdde3de3cc
hey-uu <khy2714@gmail.com>
2023-02-27 19:10:19 +0900

    Fix: world 구조체 내부 object_list -> object_arr

--- mandatory/includes/structs/s_world.h
+++ mandatory/includes/structs/s_world.h
@@ -1,12 +1,12 @@
 #ifndef S_WORLD_H
 # define S_WORLD_H
 
-# include "libft.h"
 # include "typedef.h"
+# include "s_object_arr.h"
 
 struct s_world
 {
-	t_object_list	*objects;
+	t_object_arr	objects;
 	t_color3		background_color;
 };
 
