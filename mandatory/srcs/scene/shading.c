799557c5cdc105db58e586abe1bf53e6c275f868
hey-uu <khy2714@gmail.com>
2023-02-27 19:13:08 +0900

    Feat: primary ray가 point light는 hit하지 않도록 수정

--- mandatory/srcs/scene/shading.c
+++ mandatory/srcs/scene/shading.c
@@ -1,6 +1,16 @@
 # include "scene_internal.h"
 
-t_color3	traced_color(t_ray *in_ray, t_world *world, int depth)
+static bool	hit_world(\
+					t_object_arr *objects, t_ray *in_ray, \
+					t_hit_record *h_rec, int depth)
+{
+	if (depth == 0)
+		return (hit_non_point_lights(objects, in_ray, h_rec, T_INFINITY));
+	else
+		return (hit_objects(objects, in_ray, h_rec, T_INFINITY));
+}
+
+static t_color3	traced_color(t_ray *in_ray, t_world *world, int depth)
 {
 	t_hit_record		h_rec;
 	t_scatter_record	s_rec;
@@ -8,28 +18,28 @@ t_color3	traced_color(t_ray *in_ray, t_world *world, int depth)
 	t_color3			emitted;
 	t_color3			summed;
 
-	if (depth <= 0)
+	if (depth >= MAX_DEPTH)
 		return (color3(0, 0, 0));
-	if (hit_object_list(&world->objects, in_ray, &h_rec, T_INFINITY) == false)
+	if (hit_world(&world->objects, in_ray, &h_rec, depth) == false)
 		return (world->background_color);
 	material = h_rec.material;
 	emitted = material->emitted(material, h_rec.u, h_rec.v, h_rec.p);
 	if (material->scattered(material, in_ray, &h_rec, &s_rec) == false)
 		return (emitted);
 	summed = v3_comp_wise(\
-						traced_color(&s_rec.scattered, world, depth - 1), \
+						traced_color(&s_rec.scattered, world, depth + 1), \
 						s_rec.attenuation);
 	return (v3_add(summed, emitted));
 }
 
 t_color3	sampled_color(t_scene *scene, int x, int y)
 {
-	t_ray	ray;
+	t_ray	priamary_ray;
 	double	u;
 	double	v;
 
 	u = (x + random_double()) / (scene->img->width - 1);
 	v = (y + random_double()) / (scene->img->height - 1);
-	ray = primary_ray(&scene->cam, u, v);
-	return (traced_color(&ray, &scene->world, MAX_DEPTH));
+	priamary_ray = primary_ray(&scene->cam, u, v);
+	return (traced_color(&priamary_ray, &scene->world, 0));
 }
