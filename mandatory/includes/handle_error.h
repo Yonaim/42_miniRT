6a343d4fa76d14992959d56c447bbe047dd406f7
hey-uu <khy2714@gmail.com>
2023-02-27 19:19:14 +0900

    Rename: error.h --> handle_error.h

---
+++ mandatory/includes/handle_error.h
@@ -0,0 +1,9 @@
+#ifndef HANDLE_ERROR_H
+# define HANDLE_ERROR_H
+
+# define ERRMSG_ARG_CNT "Usage: <scene_description_file_path>"
+# define ERRMSG_PARSE	"Failed to parse"
+
+int	handle_error(char *err_msg);
+
+#endif
