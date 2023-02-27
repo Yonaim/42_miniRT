cbdf5804ef907bf611751ade8244ef1c5bf3fd8d
hey-uu <khy2714@gmail.com>
2023-02-27 19:25:25 +0900

    Refactor: return 문에서 함수 호출하지 않도록 수정

--- mandatory/srcs/ray/scattered_vector.c
+++ mandatory/srcs/ray/scattered_vector.c
@@ -11,22 +11,27 @@ static double	reflectance(double cosine, double idx_ratio)
 
 t_vector3	reflected_vector(t_vector3 in, t_vector3 n)
 {
-	return (v3_sub(in, v3_mul(n, 2 * v3_dot(in, n))));
+	t_vector3	reflected;
+	
+	reflected = v3_sub(in, v3_mul(n, 2 * v3_dot(in, n)));
+	return (reflected);
 }
 
 t_vector3	refracted_vector(t_vector3 in, t_vector3 n, double idx_ratio)
 {
 	const double	cos_in = fmin(-v3_dot(in, n), 1);
 	const double	sin_in = sqrt(1 - cos_in * cos_in);
-	t_vector3		refract_perp;
-	t_vector3		refract_par;
+	t_vector3		refracted_perp;
+	t_vector3		refracted_par;
+	t_vector3		refracted;
 
 	if (idx_ratio * sin_in > 1
 		|| reflectance(cos_in, idx_ratio) > random_double())
 		return (reflected_vector(in, n));
-	refract_perp = v3_mul(v3_add(in, v3_mul(n, cos_in)), idx_ratio);
-	refract_par = v3_mul(n, -sqrt(fabs(1 - len_sqr_v3(refract_perp))));
-	return (v3_add(refract_par, refract_perp));
+	refracted_perp = v3_mul(v3_add(in, v3_mul(n, cos_in)), idx_ratio);
+	refracted_par = v3_mul(n, -sqrt(fabs(1 - len_sqr_v3(refracted_perp))));
+	refracted = v3_add(refracted_par, refracted_perp);
+	return (refracted);
 }
 
 t_vector3	diffused_vector(t_vector3 n)
