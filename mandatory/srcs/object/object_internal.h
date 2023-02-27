a1d09de9eb819a4d9720ae2d77ee088f2cb1cbf3
yona <yonaimyo@gmail.com>
2023-02-28 00:37:57 +0900

    Feat: object/determine_t 유틸 추가

--- mandatory/srcs/object/object_internal.h
+++ mandatory/srcs/object/object_internal.h
@@ -15,5 +15,6 @@ enum	e_equation_coefficient
 
 void	set_face_normal(\
 		t_hit_record *h_rec, t_ray *in_ray, t_vector3 outward_normal);
+bool	determine_t(double *t, double root[2], double t_min, double t_max);
 
 #endif
--- mandatory/srcs/object/utils.c
+++ mandatory/srcs/object/utils.c
@@ -3,6 +3,14 @@
 #include "s_ray.h"
 #include "typedef.h"
 
+/**
+ * Compare the direction of the incident ray with
+ * the direction of the outward normal vector at the hit point and record it.
+ * 
+ * In hit record, these are recorded.
+ * 	- whether or not it is opposed
+ * 	- the direction of the appropriate normal vector
+*/
 void	set_face_normal(\
 		t_hit_record *h_rec, t_ray *in_ray, t_vector3 outward_normal)
 {
@@ -16,4 +24,15 @@ void	set_face_normal(\
 		h_rec->opposed = false;
 		h_rec->normal = v3_mul(outward_normal, -1);
 	}
+}
+
+bool	determine_t(double *t, double root[2], double t_min, double t_max)
+{
+	if (is_in_range(root[0], t_min, t_max))
+		*t = root[0];
+	else if (is_in_range(root[1], t_min, t_max))
+		*t = root[1];
+	else
+		return (false);
+	return (true);
 }
