011c11ec6c43d3565af5f7117f8c6f76a75e2b1e
yona <yonaimyo@gmail.com>
2023-02-27 23:56:58 +0900

    Refator: s_object.h 구조체 멤버 순서 변경

--- mandatory/includes/structs/s_object.h
+++ mandatory/includes/structs/s_object.h
@@ -7,33 +7,33 @@
 
 struct s_object
 {
-	t_material			*material;
 	t_hit				hit;
 	t_destroy_object	destroy;
+	t_material			*material;
 };
 
 struct s_object_point_light
 {
-	t_material			*material;
 	t_hit				hit;
 	t_destroy_object	destroy;
+	t_material			*material;
 	t_point3			pos;
 };
 
 struct s_object_sphere
 {
-	t_material			*material;
 	t_hit				hit;
 	t_destroy_object	destroy;
+	t_material			*material;
 	t_point3			center;
 	double				radius;
 };
 
 struct s_object_disk
 {
-	t_material			*material;
 	t_hit				hit;
 	t_destroy_object	destroy;
+	t_material			*material;
 	t_point3			center;
 	double				radius;
 	t_vector3			normal;
@@ -41,9 +41,9 @@ struct s_object_disk
 
 struct s_object_tube
 {
-	t_material			*material;
 	t_hit				hit;
 	t_destroy_object	destroy;
+	t_material			*material;
 	t_point3			center;
 	double				radius;
 	double				height;
@@ -52,26 +52,26 @@ struct s_object_tube
 
 struct s_object_cylinder
 {
-	t_material			*material;
 	t_hit				hit;
 	t_destroy_object	destroy;
+	t_material			*material;
 	t_object_list		faces;
 };
 
 struct s_object_plane
 {
-	t_material			*material;
 	t_hit				hit;
 	t_destroy_object	destroy;
+	t_material			*material;
 	t_point3			point;
 	t_vector3			normal;
 };
 
 struct s_object_xy_rectangle
 {
-	t_material			*material;
 	t_hit				hit;
 	t_destroy_object	destroy;
+	t_material			*material;
 	double				x0;
 	double				x1;
 	double				y0;
@@ -81,9 +81,9 @@ struct s_object_xy_rectangle
 
 struct s_object_xz_rectangle
 {
-	t_material			*material;
 	t_hit				hit;
 	t_destroy_object	destroy;
+	t_material			*material;
 	double				x0;
 	double				x1;
 	double				z0;
@@ -93,9 +93,9 @@ struct s_object_xz_rectangle
 
 struct s_object_yz_rectangle
 {
-	t_material			*material;
 	t_hit				hit;
 	t_destroy_object	destroy;
+	t_material			*material;
 	double				y0;
 	double				y1;
 	double				z0;
@@ -105,9 +105,9 @@ struct s_object_yz_rectangle
 
 struct s_object_box
 {	
-	t_material			*material;
 	t_hit				hit;
 	t_destroy_object	destroy;
+	t_material			*material;
 	t_point3			p_min;
 	t_point3			p_max;
 	t_object_list		faces;
