4305a026a44dd13eb9b5b022cb1b8b0a114a1b17
hey-uu <khy2714@gmail.com>
2023-02-27 19:24:19 +0900

    Fix: 필요없는 헤더 파일 삭제

--- mandatory/includes/error.h
+++
@@ -1,9 +0,0 @@
-#ifndef ERROR_H
-# define ERROR_H
-
-# define ERRMSG_ARG_CNT "Usage: <scene_description_file_path>"
-# define ERRMSG_PARSE	"Failed to parse"
-
-int	handle_error(char *err_msg);
-
-#endif
