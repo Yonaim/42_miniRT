0f149985df4f468ab742a3840eb5c7d8a3b3f7ee
hey-uu <khy2714@gmail.com>
2023-03-01 02:42:53 +0900

    Chore: object_arr.h indent 수정

--- mandatory/includes/object_arr.h
+++ mandatory/includes/object_arr.h
@@ -7,10 +7,8 @@ int		init_object_arr(t_object_arr *objects, int cap);
 void	flush_object_arr(t_object_arr *objects);
 int		add_object(t_object_arr *objects, t_object *object);
 bool	hit_object_arr(\
-				t_object_arr *objects, \
-				t_ray *ray, \
-				t_hit_record *final_rec, \
-				double t_max);
+						t_object_arr *objects, t_ray *ray, \
+						t_hit_record *final_rec, double t_max);
 bool	hit_object_arr_except_point_light(\
 						t_object_arr *objects, t_ray *in_ray, \
 						t_hit_record *final_rec, double t_max);
