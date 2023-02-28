0b4240ca3d9d8dc917de92990da62b89d07d2e69
yonaCluster <yonaimyo@gmail.com>
2023-02-28 17:01:56 +0900

    Feat: mlx_utils 추가 및 수정

--- mandatory/includes/mlx_utils.h
+++ mandatory/includes/mlx_utils.h
@@ -4,9 +4,14 @@
 # include <mlx.h>
 # include "typedef.h"
 
+# define EVENT_KEY_PRESS	2
+# define EVENT_DESTROY		17
+# define KEY_ESC 			53
+
 t_color3	get_pixel_color(t_image *img, int x, int y);
 void		put_pixel_to_image(t_image *img, int x, int y, t_color3 color);
-void		init_mlx(t_mlx *mlx, t_image *img);
-
+void		init_mlx(t_mlx *mlx);
+int			destroy_hook(void *minirt);
+int			keypress_hook(int keycode, void *minirt);
 
 #endif
--- mandatory/srcs/utils/mlx_utils.c
+++ mandatory/srcs/utils/mlx_utils.c
@@ -2,9 +2,10 @@
 #include "libft.h"
 #include "constants.h"
 #include "s_mlx.h"
+#include "s_minirt.h"
+#include "mlx_utils.h"
+#include "scene.h"
 #include "color.h"
-
-static void	init_image(t_mlx *mlx, t_image *img);
 
 t_color3	get_pixel_color(t_image *img, int x, int y)
 {
@@ -33,20 +34,34 @@ void	put_pixel_to_image(t_image *img, int x, int y, t_color3 color)
 	}
 }
 
-void	init_mlx(t_mlx *mlx, t_image *img)
+void	init_mlx(t_mlx *mlx)
 {
+	t_image	*img;
+	
+	img = &mlx->img;
+	mlx->conn = mlx_init();
+	mlx->win = mlx_new_window(mlx->conn, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME);
 	img->width = WINDOW_WIDTH;
 	img->height = WINDOW_HEIGHT;
-	mlx->conn = mlx_init();
-	mlx->win = mlx_new_window(mlx->conn, img->width, img->height, WINDOW_NAME);
-	init_image(mlx, img);
-}
-
-static void	init_image(t_mlx *mlx, t_image *img)
-{
 	img->obj = mlx_new_image(mlx->conn, img->width, img->height);
 	img->data = mlx_get_data_addr(img->obj, &img->bits_per_pixel, \
 							&img->bytes_per_line, &img->endian);
 	img->data_size = img->bytes_per_line * img->height;
 	ft_memset(img->data, 0, img->width * img->height * img->bits_per_pixel / 8);
+}
+
+int	destroy_hook(void *minirt)
+{
+	flush_scene(&((t_minirt *)minirt)->scene);
+	exit(0);
+}
+
+int	keypress_hook(int keycode, void *minirt)
+{
+	if (keycode == KEY_ESC)
+	{
+		flush_scene(&((t_minirt *)minirt)->scene);
+		exit(0);
+	}
+	return (0);
 }
