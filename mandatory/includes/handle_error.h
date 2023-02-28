696897ac0ad4925ad90d58ad187baf0ddeb547b5
yonaCluster <yonaimyo@gmail.com>
2023-02-28 15:17:29 +0900

    Feat: 문자열상수 ERRMSG_MALLOC_FAILED 추가

--- mandatory/includes/handle_error.h
+++ mandatory/includes/handle_error.h
@@ -1,8 +1,9 @@
 #ifndef HANDLE_ERROR_H
 # define HANDLE_ERROR_H
 
-# define ERRMSG_ARG_CNT "Usage: <scene_description_file_path>"
-# define ERRMSG_PARSE	"Failed to parse"
+# define ERRMSG_ARG_CNT 		"Usage: <scene_description_file_path>"
+# define ERRMSG_PARSE			"Failed to parse"
+# define ERRMSG_MALLOC_FAILED	"Malloc failed"
 
 int	handle_error(char *err_msg);
 
