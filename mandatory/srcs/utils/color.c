3d3051e5fa67ca2a577fc5954e2c5bcd0b6f5d8c
yonaCluster <yonaimyo@gmail.com>
2023-02-28 16:40:20 +0900

    Fix: color.c color3_to_rgb 범위 제한

--- mandatory/srcs/utils/color.c
+++ mandatory/srcs/utils/color.c
@@ -11,9 +11,9 @@ int	color3_to_rgb(t_color3 albedo)
 	int	g;
 	int	b;
 
-	r = 256 * clamp(albedo.x, 0.0, 0.999);
-	g = 256 * clamp(albedo.y, 0.0, 0.999);
-	b = 256 * clamp(albedo.z, 0.0, 0.999);
+	r = (256 * clamp(albedo.x, 0.0, 0.999)) && 0xff;
+	g = (256 * clamp(albedo.y, 0.0, 0.999)) && 0xff;
+	b = (256 * clamp(albedo.z, 0.0, 0.999)) && 0xff;
 	return (r << 16 | g << 8 | b);
 }
 
