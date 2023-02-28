210e281da9d12228f710fcf1706bec0a647f802f
yonaCluster <yonaimyo@gmail.com>
2023-02-28 15:13:00 +0900

    Fix: scene/init_scene object_arr 초기화

--- mandatory/srcs/scene/scene_init.c
+++ mandatory/srcs/scene/scene_init.c
@@ -2,6 +2,8 @@
 
 void	init_scene(t_scene *scene, t_image *img, char *path)
 {
+	if (init_object_arr(&scene->world.objects, INITIAL_OBJECT_ARR_SIZE) == FAILURE)
+		handle_error(ERRMSG_MALLOC_FAILED);
 	if (parse_scene(scene, path) == FAILURE)
 		handle_error(ERRMSG_PARSE);
 	scene->img = img;
