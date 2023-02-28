df019abe019007fa96cbcb558b72d9c2d2d9c984
hey-uu <khy2714@gmail.com>
2023-03-01 01:36:42 +0900

    Fix: scene/shading.c traced_color에 pdf 계산 추가

--- mandatory/srcs/scene/shading.c
+++ mandatory/srcs/scene/shading.c
@@ -12,7 +12,7 @@ static bool	hit_world(
 	return (false);
 }
 
-t_color3	traced_color(t_ray *in_ray, t_world *world, int depth)
+t_color3	traced_color(t_ray *ray, t_world *world, int depth)
 {
 	t_hit_record		h_rec;
 	t_scatter_record	s_rec;
@@ -22,18 +22,16 @@ t_color3	traced_color(t_ray *in_ray, t_world *world, int depth)
 
 	if (depth >= MAX_DEPTH)
 		return (color3(0, 0, 0));
-	if (hit_world(&world->objects, in_ray, &h_rec, depth) == false)
+	if (hit_world(&world->objects, ray, &h_rec, depth) == false)
 		return (world->background_color);
 	material = h_rec.material;
-	emitted = material->emitted(material, h_rec.u, h_rec.v, h_rec.p);
-	if (material->scattered(material, in_ray, &h_rec, &s_rec) == false)
+	emitted = material->emitted(material, &h_rec);
+	if (material->scattered(material, ray, &h_rec, &s_rec) == false)
 		return (emitted);
-	summed = v3_comp_wise(\
-						traced_color(&s_rec.scattered, world, depth + 1), \
+	summed = v3_comp_wise(traced_color(&s_rec.scattered, world, depth + 1), \
 						s_rec.albedo);
-	summed = v3_mul(summed,
-					h_rec.material->s_pdf(
-						h_rec.material, in_ray, &h_rec, &s_rec.scattered)
+	summed = v3_mul(summed, \
+					material->s_pdf(material, ray, &h_rec, &s_rec.scattered)
 					/ s_rec.pdf_val);
 	return (v3_add(summed, emitted));
 }
