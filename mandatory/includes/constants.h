9a2ff2e225f026b8175782e9e7af4030b5856476
yonaCluster <yonaimyo@gmail.com>
2023-02-28 15:15:56 +0900

    Feat: 매크로 상수 INITIAL_OBJECT_ARR_SIZE  추가

--- mandatory/includes/constants.h
+++ mandatory/includes/constants.h
@@ -10,37 +10,40 @@ enum e_success_or_failure
 };
 
 // window
-# define WINDOW_NAME			"meyun & yona's wonderful miniRT"
-# define WINDOW_WIDTH			1920
-# define WINDOW_HEIGHT			1080
+# define WINDOW_NAME				"meyun & yona's wonderful miniRT"
+# define WINDOW_WIDTH				1920
+# define WINDOW_HEIGHT				1080
 
 // t range
-# define T_MINIMUM				0.001
-# define T_INFINITY 			INT_MAX
+# define T_MINIMUM					0.001
+# define T_INFINITY 				INT_MAX
 
 // sampling
-# define SAMPLES_PER_PIXEL		10
-# define PUT_IMG_TERM			10
+# define SAMPLES_PER_PIXEL			10
+# define PUT_IMG_TERM				10
 
 // recursive depth
-# define MAX_DEPTH				50
+# define MAX_DEPTH					50
 
 // camera
-# define APERTURE				0
-# define FOCAL_DISTANCE			10.0
-# define UP_VECTOR				(t_vector3){0, 1, 0}
+# define APERTURE					0
+# define FOCAL_DISTANCE				10.0
+# define UP_VECTOR					(t_vector3){0, 1, 0}
 
 // object
-# define POINT_LIGHT_RADIUS		5
+# define POINT_LIGHT_RADIUS			5
+
+// object array
+# define INITIAL_OBJECT_ARR_SIZE	10
 /** dev **/
 
 // object
-# define MATERIAL_NUMBER		4
-# define OBJECT_NUMBER			5
+# define MATERIAL_NUMBER			4
+# define OBJECT_NUMBER				5
 
 // camera
-# define FIELD_OF_VIEW			30
-# define LOOK_FROM				(t_point3){13, 2, 3}
-# define LOOK_AT				(t_point3){0, 0, 0}
+# define FIELD_OF_VIEW				30
+# define LOOK_FROM					(t_point3){13, 2, 3}
+# define LOOK_AT					(t_point3){0, 0, 0}
 
 #endif
