179b6ae18237bcca9c95bd73ee8e9c82520291c1
yonaCluster <yonaimyo@gmail.com>
2023-02-28 15:20:07 +0900

    Fix: object.h 함수 원형 선언 수정

--- mandatory/includes/object.h
+++ mandatory/includes/object.h
@@ -5,16 +5,13 @@
 # include "s_hit_record.h"
 
 // constructor
-t_object	*new_sphere(t_point3 pos, double radius, t_color3 rgb);
-t_object	*new_cylinder(t_point3 pos, t_vector3 orient, \
-							double radius, double height, t_color3 rgb);
-								// 파라미터 수 4개 이하로 줄여야 함
-t_object	*new_tube(t_point3 center, t_vector3 orient, \
-							double radius, double height, t_color3 rgb);
-								// 파라미터 수 4개 이하로 줄여야 함
+t_object	*new_sphere(t_point3 center, double radius, t_material *material);
+t_object	*new_cylinder(t_object_tube *tube, t_object_disk *disk[2]);
 t_object	*new_plane(t_point3 pos, t_vector3 normal, t_color3 rgb);
-t_object	*new_disk(t_point3 center, double radius, t_vector3 normal, t_color3 rgb);
-t_object	*new_tube(t_point3 center, double radius, t_vector3 normal, t_color3 rgb);
+t_object	*new_disk(t_point3 center, double radius, \
+								t_vector3 normal, t_material *material);
+t_object	*new_tube(t_object_disk *disk, t_vector3 orient, \
+							double height, t_material *material);
 t_object	*new_light(t_point3 pos, double ratio, t_color3 rgb);
 
 // hit check
