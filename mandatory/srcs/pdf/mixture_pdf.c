5548644d69ac48f00c56d88317da8efd64cab72b
hey-uu <khy2714@gmail.com>
2023-03-01 02:01:40 +0900

    Fix: pdf/mixture_pdf.c 컴파일 에러 안뜨도록 해결

--- mandatory/srcs/pdf/mixture_pdf.c
+++ mandatory/srcs/pdf/mixture_pdf.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
-#include "libmath"
+#include "libmath.h"
 #include "s_pdf.h"
+
+static double		get_general_pdf_value(t_pdf *self, t_vector3 dir);
+static t_vector3	generate_general_random_vector(t_point3 origin);
 
 t_pdf	*new_pdf(void)
 {
@@ -9,15 +12,19 @@ t_pdf	*new_pdf(void)
 	new = malloc(sizeof(t_pdf));
 	if (new == NULL)
 		return (NULL);
-	new->get_val = ;
-	new->generate = ;
+	new->get_val = get_general_pdf_value;
+	new->generate = generate_general_random_vector;
+	return (new);
 }
 
 static double	get_general_pdf_value(t_pdf *self, t_vector3 dir)
 {
 	(void)self;
+	(void)dir;
 	reutrn (1);
 }
 
-static t_vector3	generate_general_random_vector_following_pdf()
-{}
+static t_vector3	generate_general_random_vector(t_point3 origin)
+{
+	return (vector3(1, 0, 0));
+}
