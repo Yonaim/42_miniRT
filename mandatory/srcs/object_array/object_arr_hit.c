81b007d4e38026a27b0afe53424658d2b2c5f438
hey-uu <khy2714@gmail.com>
2023-02-28 21:13:00 +0900

    Refactor: 조건문 위치 변경

--- mandatory/srcs/object_array/object_arr_hit.c
+++ mandatory/srcs/object_array/object_arr_hit.c
@@ -1,5 +1,6 @@
 #include "object_arr_internal.h"
 #include <stdio.h>
+
 bool	hit_object_arr_except_point_light(\
 						t_object_arr *objects, t_ray *in_ray, \
 						t_hit_record *final_rec, double t_max)
@@ -16,15 +17,14 @@ bool	hit_object_arr_except_point_light(\
 	while (i < objects->cnt)
 	{
 		cur_obj = objects->arr[i];
-		i++;
-		if (cur_obj->get_type() == OBJECT_POINT_LIGHT)
-			continue ;
-		if (cur_obj->hit(cur_obj, in_ray, &cur_rec, closest_t) == true)
+		if (cur_obj->get_type() != OBJECT_POINT_LIGHT
+			&& cur_obj->hit(cur_obj, in_ray, &cur_rec, closest_t) == true)
 		{
 			is_hit = true;
 			*final_rec = cur_rec;
 			closest_t = cur_rec.t;
 		}
+		i++;
 	}
 	return (is_hit);
 }
