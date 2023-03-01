c2c1cd7464ba0b572eee2d6d39fc4d85f2781eaf
hey-uu <khy2714@gmail.com>
2023-03-01 18:54:16 +0900

    Feat: texture.c new_texture  함수 추가

---
+++ mandatory/srcs/texture/texture.c
@@ -0,0 +1,21 @@
+#include "texture_internal.h"
+#include "s_info_element.h"
+
+t_texture	*new_texture(t_info_texture *texture_info)
+{
+	t_texture	*new;
+
+	if (texture_info->type == TEXTURE_SOLID)
+	{
+		return (new_solid(texture_info->rgb1));
+	}
+	else if (texture_info->type == TEXTURE_CHECKER)
+	{
+		return (new_checker_solid(texture_info->rgb1, texture_info->rgb2));
+	}
+	else if (texture_info->type == TEXTURE_IMAGE)
+	{
+		return (NULL);
+	}
+	return (NULL);
+}
