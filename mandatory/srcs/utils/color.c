06e70bb2aa9f0541611d3ccc93a9ffc86c10440d
hey-uu <khy2714@gmail.com>
2023-03-01 16:03:09 +0900

    Fix: color3_to_rgb 매개변수 포인터로 수정

--- mandatory/srcs/utils/color.c
+++ mandatory/srcs/utils/color.c
@@ -5,15 +5,15 @@ t_color3	color3(double r, double g, double b)
 	return ((t_color3)vector3(r, g, b));
 }
 
-int	color3_to_rgb(t_color3 albedo)
+int	color3_to_rgb(t_color3 *albedo)
 {
 	int	r;
 	int	g;
 	int	b;
 
-	r = (int)(256 * clamp(albedo.x, 0.0, 0.999)) & 0xff;
-	g = (int)(256 * clamp(albedo.y, 0.0, 0.999)) & 0xff;
-	b = (int)(256 * clamp(albedo.z, 0.0, 0.999)) & 0xff;
+	r = (int)(256 * clamp((*albedo).x, 0.0, 0.999)) & 0xff;
+	g = (int)(256 * clamp((*albedo).y, 0.0, 0.999)) & 0xff;
+	b = (int)(256 * clamp((*albedo).z, 0.0, 0.999)) & 0xff;
 	return (r << 16 | g << 8 | b);
 }
 
