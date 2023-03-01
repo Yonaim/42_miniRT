140f89eb84973a322cda1666e4f23b9c79fc6397
hey-uu <khy2714@gmail.com>
2023-03-01 18:54:50 +0900

    Fix: 필요한 함수 프로토타입 추가

--- mandatory/includes/texture.h
+++ mandatory/includes/texture.h
@@ -3,6 +3,7 @@
 
 # include "typedef.h"
 
+t_texture	*new_texture(t_info_texture *texture_info);
 t_texture	*new_solid(t_color3 color);
 t_texture	*new_checker_solid(t_color3 even, t_color3 odd);
 t_texture	*new_checker(t_texture *even, t_texture *odd);
--- mandatory/srcs/material/material_internal.h
+++ mandatory/srcs/material/material_internal.h
@@ -25,5 +25,7 @@
 t_material	*new_lambertian(t_texture *texture);
 t_material	*new_dielectric(double refractive_idx);
 t_material	*new_metal(t_color3 albedo, double fuzz);
+t_material	*new_emmisive(t_color3 color);
+t_material	*new_random_material(void);
 
 #endif
