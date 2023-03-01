16f514500ca93b7e74baf1774863eb12e8d53647
hey-uu <khy2714@gmail.com>
2023-03-01 15:04:05 +0900

    Struct: mlx 관련 함수들 두 파일로 나누기

--- mandatory/srcs/utils/mlx_utils.c
+++ mandatory/srcs/utils/mlx_utils.c
@@ -7,7 +7,7 @@
 #include "scene.h"
 #include "color.h"
 
-t_color3	get_pixel_color(t_image *img, int x, int y)
+t_color3	get_pixel_color(t_image *img, char *data, int x, int y)
 {
 	char		*dst;
 	int			offset;
@@ -17,7 +17,7 @@ t_color3	get_pixel_color(t_image *img, int x, int y)
 	offset = (y * img->bytes_per_line) + (x * (img->bits_per_pixel / 8));
 	if (offset <= 0 || offset > (img->width * img->height) * 4)
 		return (color3(0, 0, 0));
-	dst = &img->data[offset];
+	dst = &data[offset];
 	return (rgb_to_color3(*(unsigned int *)dst));
 }
 
@@ -54,31 +54,18 @@ void	update_buff_pixel_color(t_image *img, int x, int y, t_color3 color)
 void	init_mlx(t_mlx *mlx)
 {
 	t_image	*img;
-	
+
 	img = &mlx->img;
 	mlx->conn = mlx_init();
-	mlx->win = mlx_new_window(mlx->conn, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME);
+	mlx->win = mlx_new_window(\
+					mlx->conn, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME);
 	img->width = WINDOW_WIDTH;
 	img->height = WINDOW_HEIGHT;
 	img->obj = mlx_new_image(mlx->conn, img->width, img->height);
 	img->data = mlx_get_data_addr(img->obj, &img->bits_per_pixel, \
 							&img->bytes_per_line, &img->endian);
+	img->buff = ft_calloc(img->height * img->width, sizeof(char));
 	img->data_size = img->bytes_per_line * img->height;
 	ft_memset(img->data, 0, img->width * img->height * img->bits_per_pixel / 8);
 }
 
-int	destroy_hook(void *minirt)
-{
-	flush_scene(&((t_minirt *)minirt)->scene);
-	exit(0);
-}
-
-int	keypress_hook(int keycode, void *minirt)
-{
-	if (keycode == KEY_ESC)
-	{
-		flush_scene(&((t_minirt *)minirt)->scene);
-		exit(0);
-	}
-	return (0);
-}
