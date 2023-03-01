b8a93b37f6ba4077a8d51e84762b764bad9c12f0
hey-uu <khy2714@gmail.com>
2023-03-01 16:13:10 +0900

    Refactor: scene_render.c render_one_layer 변경

--- mandatory/srcs/scene/scene_render.c
+++ mandatory/srcs/scene/scene_render.c
@@ -1,17 +1,9 @@
 #include "scene_internal.h"
-#include <stdio.h>
-
-void	gamma_correction(t_color3 *color)
-{
-	color->x = sqrt(color->x);
-	color->y = sqrt(color->y);
-	color->z = sqrt(color->z);
-}
 
 void	render_one_layer(t_scene *scene, int layer_cnt)
 {
-	t_color3	old_color;
-	t_color3	new_color;
+	t_color3	sampled_color;
+	t_color3	stored_color;
 	int			x;
 	int			y;
 
@@ -21,14 +13,12 @@ void	render_one_layer(t_scene *scene, int layer_cnt)
 		y = 0;
 		while (y < scene->img->height)
 		{
-			new_color = sampled_color(scene, x, y);
-			old_color = get_pixel_color(scene->img, scene->img->buff, x, y);
-			new_color = v3_div(\
-							v3_add(v3_mul(old_color, layer_cnt), new_color), \
-							(layer_cnt + 1));
-			update_buff_pixel_color(scene->img, x, y, new_color);
-			gamma_correction(&new_color);
-			put_pixel_to_image(scene->img, x, y, new_color);
+			get_sampled_color(&sampled_color, scene, x, y);
+			get_stored_color(&stored_color, scene->img, x, y);
+			calculate_average_color(&stored_color, &sampled_color, layer_cnt);
+			update_stored_color(scene->img, &stored_color, x, y);
+			gamma_correction(&stored_color);
+			put_pixel_to_image(scene->img, &stored_color, x, y);
 			y++;
 		}
 		x++;
