c1264677172888b91a3f00dbfdc113c24f774a60
yonaCluster <yonaimyo@gmail.com>
2023-03-03 16:56:02 +0900

    Feat: /parse/ 메인 로직 완성

---
+++ mandatory/srcs/parse/parse.c
@@ -0,0 +1,119 @@
+#include "parse_internal.h"
+#include <string.h>
+
+static int	match_element_line_format(t_token *tokens)
+{
+	const t_is_element_line	is_element_line[] = {
+	[ELEMENT_CAMERA] = is_camera_line,
+	[ELEMENT_AMBIENT] = is_ambient_line,
+	[ELEMENT_LIGHT] = is_light_line,
+	[ELEMENT_SPHERE] = is_sphere_line,
+	[ELEMENT_PLANE] = is_plane_line,
+	[ELEMENT_CYLINDER] = is_cylinder_line,
+	[ELEMENT_CONE] = is_cone_line,
+	[ELEMENT_BOX] = is_box_line,
+	};
+	int						idx;
+
+	idx = 0;
+	while (idx < ELEMENT_TYPE_COUNT)
+	{
+		if (is_element_line[idx](tokens) == true)
+			return (idx);
+		idx++;
+	}
+	return (-1);
+}
+
+static int	add_element(int type, t_token_arr *tokens)
+{
+	const t_get_info_element get_info_elem[] = {
+	[ELEMENT_CAMERA] = get_info_camera,
+	[ELEMENT_AMBIENT] = get_info_ambient,
+	[ELEMENT_LIGHT] = get_info_light,
+	[ELEMENT_SPHERE] = get_info_sphere,
+	[ELEMENT_PLANE] = get_info_plane,
+	[ELEMENT_CYLINDER] = get_info_cylinder,
+	[ELEMENT_CONE] = get_info_cone,
+	[ELEMENT_BOX] = get_info_box,
+	};
+	const t_info			elem_info = get_info_elem[type](tokens);
+
+	if (elem_info == NULL)
+		return (FAILURE);
+	return (SUCCESS);
+}
+
+static bool	is_must_be_one_element_type(int type)
+{
+	const int	must_be_one[] = {
+		ELEMENT_AMBIENT,
+		ELEMENT_CAMERA,
+		ELEMENT_LIGHT,
+		ELEMENT_NONE
+	};
+	int			i;
+
+	while (must_be_one[i] != ELEMENT_NONE)
+	{
+		if (type == must_be_one[i])
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+static bool	is_must_be_elements_exist(int elem_exist[])
+{
+	int	i;
+
+	while (i < ELEMENT_TYPE_COUNT)
+	{
+		if (is_must_be_one_element_type(i) == true \
+			&& elem_exist[i] == 0)
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+static int	parse_line(t_scene *scene, char *line, bool elem_exist[])
+{
+
+	const t_token_arr		*tokens = tokenize(line);
+	int						type;
+
+	if (tokens == NULL)
+		return (FAILURE);
+	type = match_element_line_format(tokens);
+	if (type == -1)
+		return (FAILURE);
+	if (is_must_be_one_element_type(type) == true \
+		&& elem_exist[type] == true)
+		handle_error("Duplicate element exists that must exist only one.");
+	if (add_element(type, tokens) == FAILURE)
+		return (FAILURE);
+	elem_exist[type] = true;
+	return (SUCCESS);
+}
+
+int	parse_scene(t_scene *scene, char *path)
+{
+	const int	fd = open(path, O_RDONLY);
+	char		*line;
+	bool		elem_exist[ELEMENT_TYPE_COUNT];
+
+	if (fd < 0)
+		return (FAILURE);
+	ft_memset(elem_exist, false, sizeof(bool) * ELEMENT_TYPE_COUNT);
+	line = get_next_line(fd);
+	while (line)
+	{
+		if (parse_line(scene, line, elem_exist) == FAILURE)
+			return (FAILURE);
+		line = get_next_line(fd);
+	}
+	if (is_must_be_elements_exist(elem_exist) == false)
+		handle_error("There is no element that must exist.");
+	return (SUCCESS);
+}
---
+++ mandatory/srcs/parse/parse_internal.h
@@ -0,0 +1,98 @@
+#ifndef PARSER_H
+# define PARSER_H
+
+// standard library
+# include <math.h>
+# include <fcntl.h>
+# include <stdlib.h>
+// extra
+# include "libft.h"
+# include "constants.h"
+# include "handle_error.h"
+# include "typedef.h"
+// structures
+# include "s_scene.h"
+# include "s_mlx.h"
+# include "s_hit_record.h"
+# include "s_scatter_record.h"
+# include "s_material.h"
+# include "s_object.h"
+# include "s_ray.h"
+// prototypes
+# include "mlx_utils.h"
+# include "ray.h"
+# include "parsing.h"
+# include "camera.h"
+# include "object_arr.h"
+# include "color.h"
+# include "dynamic_arr.h"
+
+# include "s_token.h"
+# include "s_info_element.h"
+
+enum e_rgb
+{
+	R,
+	G,
+	B
+};
+
+# define IDENTIFIER_ELEMENT_CAMERA		"C"
+# define IDENTIFIER_ELEMENT_AMBIENT		"A"
+# define IDENTIFIER_ELEMENT_LIGHT		"L"
+# define IDENTIFIER_ELEMENT_SPHERE		"sp"
+# define IDENTIFIER_ELEMENT_PLANE		"pl"
+# define IDENTIFIER_ELEMENT_CYLINDER	"cy"
+# define IDENTIFIER_ELEMENT_CONE		"co"
+# define IDENTIFIER_ELEMENT_BOX			"bo"
+
+# define IDENTIFIER_MATERIAL_LAMBERTIAL	"l"
+# define IDENTIFIER_MATERIAL_METAL		"m"
+# define IDENTIFIER_MATERIAL_DIELECTRIC	"d"
+# define IDENTIFIER_MATERIAL_EMMISIVE	"e"
+# define IDENTIFIER_MATERIAL_RANDOM		"rand"
+
+# define IDENTIFIER_TEXTURE_SOLID		"s"
+# define IDENTIFIER_TEXTURE_CHECKER		"c"
+# define IDENTIFIER_TEXTURE_IMAGE		"i"
+
+typedef t_dynamic_arr	t_token_arr;
+
+typedef t_info			(*t_get_info_element)(t_token *tokens);
+typedef int				(*t_get_identifier_type)(const char *s);
+typedef	bool	 		(*t_is_element_line)(t_token *tokens);
+
+t_token_arr	*tokenize(char *line);
+int			get_token_type(const char *input);
+int			length_of_word(const char *line);
+int			length_of_number(const char *line);
+
+int			get_element_type(const char *str);
+int			get_material_type(const char *str);
+int			get_texture_type(const char *str);
+
+t_info		get_info_ambient(t_token *tokens);
+t_info		get_info_camera(t_token *tokens);
+t_info		get_info_light(t_token *tokens);
+t_info		get_info_plane(t_token *tokens);
+t_info		get_info_sphere(t_token *tokens);
+t_info		get_info_cylinder(t_token *tokens);
+t_info		get_info_cone(t_token *tokens);
+t_info		get_info_box(t_token *tokens);
+
+bool		is_ambient_line(t_token_arr *tokens);
+bool		is_camera_line(t_token_arr *tokens);
+bool		is_light_line(t_token_arr *tokens);
+bool		is_plane_line(t_token_arr *tokens);
+bool		is_sphere_line(t_token_arr *tokens);
+bool		is_cylinder_line(t_token_arr *tokens);
+bool		is_cone_line(t_token_arr *tokens);
+bool		is_box_line(t_token_arr *tokens);
+
+bool		is_valid_formatted_line(char *identifier, \
+								int *form, t_token_arr *tokens);
+bool		is_space(char c);
+bool		is_number_str(char *s);
+bool		is_identifier_str(char *s);
+
+#endif
