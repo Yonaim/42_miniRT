d0a7c87a5214344a75e9d99c72e08417c02e9eaf
hey-uu <khy2714@gmail.com>
2023-03-03 14:00:32 +0900

    Chore: scene/manage_color  역슬래시 제거

--- mandatory/srcs/scene/manage_color.c
+++ mandatory/srcs/scene/manage_color.c
@@ -19,15 +19,15 @@ void	get_stored_color(t_color3 *stored_color, t_image *img, int x, int y)
 	*stored_color = img->tmp_data[offset];
 }
 
-void	calculate_average_color(\
-			t_color3 *stored_color, t_color3 *sampled_color, int stored_cnt)
+void	calculate_average_color(
+		t_color3 *stored_color, t_color3 *sampled_color, int stored_cnt)
 {
 	*stored_color = v3_add(v3_mul(*stored_color, stored_cnt), *sampled_color);
 	*stored_color = v3_div(*stored_color, stored_cnt + 1);
 }
 
-void	update_stored_color(\
-			t_image *img, t_color3 *stored_color, int x, int y)
+void	update_stored_color(
+		t_image *img, t_color3 *stored_color, int x, int y)
 {
 	const int	offset = y * img->width + x;
 
