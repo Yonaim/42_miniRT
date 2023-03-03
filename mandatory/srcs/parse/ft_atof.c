5d19c511055fb08dd8229bf82d544582608058b9
yonaCluster <yonaimyo@gmail.com>
2023-03-03 14:40:43 +0900

    Feat: ft_atof

---
+++ mandatory/srcs/parse/ft_atof.c
@@ -0,0 +1,38 @@
+#include "libft.h"
+
+static double	ft_atof_fractional_part(char *str)
+{
+	double	n;
+	double	unit_n;
+	
+	n = 0;
+	unit_n = 0.1;
+	while (ft_isdigit(*str))
+	{
+		n += unit_n * (*str - '0');
+		unit_n /= 10;
+		str++;
+	}
+	return (n);
+}
+
+double	ft_atof(char *str)
+{
+	int		point_left;
+	double	point_right;
+	
+	point_left = ft_atoi(str);
+	point_right = 0;
+	while (*str == ' ' || *str == '\t')
+		str++;
+	if (*str == '+' || *str == '-')
+		str++;
+	while (ft_isdigit(*str))
+		str++;
+	if (*str == '.')
+		point_right = ft_atof_fractional_part(++str);
+	if (point_left < 0)
+		return ((double)(point_left + (point_right * -1)));
+	else
+		return ((double)(point_left + point_right));
+}
