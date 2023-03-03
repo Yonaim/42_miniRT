15a7dbb99a6c2ee1e29f870cec9629e3c7a2d079
hey-uu <khy2714@gmail.com>
2023-03-03 13:51:22 +0900

    Chore: material/ 함수 줄바꿈하는 형식 수정

--- mandatory/srcs/material/lambertian.c
+++ mandatory/srcs/material/lambertian.c
@@ -1,16 +1,15 @@
 
 #include "material_internal.h"
 
-static bool		lambertian_scattered(\
-							t_material *self, t_ray *in, \
-							t_hit_record *h_rec, t_scatter_record *s_rec);
-static t_color3	lambertian_emitted(\
-								t_material *self, \
-								double u, double v, t_point3 p);
+static bool		lambertian_scattered(
+					t_material *self, t_ray *in, 
+					t_hit_record *h_rec, t_scatter_record *s_rec);
+static t_color3	lambertian_emitted(
+					t_material *self, double u, double v, t_point3 p);
 static void		destroy_lambertian(t_material *self);
-static double	get_scattering_pdf(\
-								t_material *self, t_ray *in_ray, \
-								t_hit_record *h_rec, t_scatter_record *s_rec);
+static double	get_scattering_pdf(
+					t_material *self, t_ray *in_ray, 
+					t_hit_record *h_rec, t_scatter_record *s_rec);
 
 t_material	*new_lambertian(t_texture *texture)
 {
@@ -27,9 +26,9 @@ t_material	*new_lambertian(t_texture *texture)
 	return ((t_material *)lambertian);
 }
 
-static bool	lambertian_scattered(\
-							t_material *self, t_ray *in, \
-							t_hit_record *h_rec, t_scatter_record *s_rec)
+static bool	lambertian_scattered(
+			t_material *self, t_ray *in,
+			t_hit_record *h_rec, t_scatter_record *s_rec)
 {
 	const t_material_lambertian	*lambertian = (t_material_lambertian *)self;
 	t_texture_solid				*solid;
@@ -38,15 +37,14 @@ static bool	lambertian_scattered(\
 	h_rec->onb = orthonormal_basis(h_rec->normal);
 	s_rec->scattered = diffused_ray(h_rec);
 	solid = (t_texture_solid *)lambertian->albedo;
-	s_rec->attenuation = solid->get_val(\
-						(t_texture *)solid, h_rec->u, h_rec->v, h_rec->p);
+	s_rec->attenuation = solid->get_val((t_texture *)solid, \
+										h_rec->u, h_rec->v, h_rec->p);
 	s_rec->pdf_val = v3_dot(h_rec->onb.w, s_rec->scattered.dir) / M_PI;
 	return (true);
 }
 
-static t_color3	lambertian_emitted(\
-								t_material *self, \
-								double u, double v, t_point3 p)
+static t_color3	lambertian_emitted(
+				t_material *self, double u, double v, t_point3 p)
 {
 	(void)self;
 	(void)u;
@@ -64,13 +62,12 @@ static void	destroy_lambertian(t_material *self)
 	free(lambertian);
 }
 
-static double	get_scattering_pdf(\
-								t_material *self, t_ray *in_ray, \
-								t_hit_record *h_rec, t_scatter_record *s_rec)
+static double	get_scattering_pdf(
+				t_material *self, t_ray *in_ray,
+				t_hit_record *h_rec, t_scatter_record *s_rec)
 {
-	const double	cosine = v3_dot(\
-									h_rec->normal, \
-									v3_normalize(s_rec->scattered.dir));
+	const double	cosine = v3_dot(h_rec->normal, \
+								v3_normalize(s_rec->scattered.dir));
 
 	(void)self;
 	(void)in_ray;
