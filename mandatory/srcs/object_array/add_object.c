8507e13999e0a9b7d0dd1ec1778f03ce694bc3f8
hey-uu <khy2714@gmail.com>
2023-03-03 19:09:14 +0900

    Feat: object_array/ add_n_objects 함수 추추가

--- mandatory/srcs/object_array/add_object.c
+++ mandatory/srcs/object_array/add_object.c
@@ -1,10 +1,24 @@
 #include "object_arr_internal.h"
 
-int	add_object(t_object_arr *objects, t_object *object)
+int	add_object(t_object_arr *object_arr, t_object *object)
 {
 	if (object == NULL)
 		return (FAILURE);
-	if (push_back_dynamic_arr(objects, object) == FAILURE)
+	if (push_back_dynamic_arr(object_arr, object) == FAILURE)
 		return (FAILURE);
+	return (SUCCESS);
+}
+
+int	add_n_objects(t_object_arr *object_arr, t_object **objects, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (add_object(object_arr, objects[i]) == FAILURE)
+			return (FAILURE);
+		i++;
+	}
 	return (SUCCESS);
 }
