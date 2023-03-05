525b78f5b6b8cff9a6b6692d3a77d464861e129f
hey-uu <khy2714@gmail.com>
2023-03-05 14:04:51 +0900

    Fix: parsing/build_element_box 오타 수정

--- mandatory/srcs/parsing/build_element_box.c
+++ mandatory/srcs/parsing/build_element_box.c
@@ -8,14 +8,14 @@ void	_build_element_box_xy_rectangle(
 	bo_info->xy_rectangle[0].texture = bo_info->texture;
 	bo_info->xy_rectangle[0].x1 = p_end1->x;
 	bo_info->xy_rectangle[0].x2 = p_end2->x;
-	bo_info->xy_rectangle[0].y2 = p_end1->y;
+	bo_info->xy_rectangle[0].y1 = p_end1->y;
 	bo_info->xy_rectangle[0].y2 = p_end2->y;
 	bo_info->xy_rectangle[0].z = p_end1->z;
 	bo_info->xy_rectangle[1].material = bo_info->material;
 	bo_info->xy_rectangle[1].texture = bo_info->texture;
 	bo_info->xy_rectangle[1].x1 = p_end1->x;
 	bo_info->xy_rectangle[1].x2 = p_end2->x;
-	bo_info->xy_rectangle[1].y2 = p_end1->y;
+	bo_info->xy_rectangle[1].y1 = p_end1->y;
 	bo_info->xy_rectangle[1].y2 = p_end2->y;
 	bo_info->xy_rectangle[1].z = p_end2->z;
 }
@@ -28,14 +28,14 @@ void	_build_element_box_xz_rectangle(
 	bo_info->xz_rectangle[0].texture = bo_info->texture;
 	bo_info->xz_rectangle[0].x1 = p_end1->x;
 	bo_info->xz_rectangle[0].x2 = p_end2->x;
-	bo_info->xz_rectangle[0].z2 = p_end1->z;
+	bo_info->xz_rectangle[0].z1 = p_end1->z;
 	bo_info->xz_rectangle[0].z2 = p_end2->z;
 	bo_info->xz_rectangle[0].y = p_end1->y;
 	bo_info->xz_rectangle[1].material = bo_info->material;
 	bo_info->xz_rectangle[1].texture = bo_info->texture;
 	bo_info->xz_rectangle[1].x1 = p_end1->x;
 	bo_info->xz_rectangle[1].x2 = p_end2->x;
-	bo_info->xz_rectangle[1].z2 = p_end1->z;
+	bo_info->xz_rectangle[1].z1 = p_end1->z;
 	bo_info->xz_rectangle[1].z2 = p_end2->z;
 	bo_info->xz_rectangle[1].y = p_end2->y;
 }
@@ -48,14 +48,14 @@ void	_build_element_box_yz_rectangle(
 	bo_info->yz_rectangle[0].texture = bo_info->texture;
 	bo_info->yz_rectangle[0].y1 = p_end1->y;
 	bo_info->yz_rectangle[0].y2 = p_end2->y;
-	bo_info->yz_rectangle[0].z2 = p_end1->z;
+	bo_info->yz_rectangle[0].z1 = p_end1->z;
 	bo_info->yz_rectangle[0].z2 = p_end2->z;
 	bo_info->yz_rectangle[0].x = p_end1->x;
 	bo_info->yz_rectangle[1].material = bo_info->material;
 	bo_info->yz_rectangle[1].texture = bo_info->texture;
 	bo_info->yz_rectangle[1].y1 = p_end1->y;
 	bo_info->yz_rectangle[1].y2 = p_end2->y;
-	bo_info->yz_rectangle[1].y2 = p_end1->y;
+	bo_info->yz_rectangle[1].y1 = p_end1->y;
 	bo_info->yz_rectangle[1].y2 = p_end2->y;
 	bo_info->yz_rectangle[1].x = p_end2->x;
 }
