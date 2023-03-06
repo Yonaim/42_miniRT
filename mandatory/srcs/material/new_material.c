b42b8466eac40cb86edc56f5339535bec2ec38e3
yonaMac <yonaimyo@gmail.com>
2023-03-07 01:20:08 +0900

    Refactor: material_ -> matl_

--- mandatory/srcs/material/new_material.c
+++ mandatory/srcs/material/new_material.c
@@ -2,19 +2,19 @@
 #include "s_info_element.h"
 
 // texture type이 TEXTURE_NONE인 경우 texture 포인터에 널 포인터가 저장된다.
-t_material	*new_material(const t_info_material *material_info)
+t_material	*new_material(const t_info_material *matl_info)
 {
-	const t_texture	*texture = new_texture(&material_info->texture);
+	const t_texture	*texture = new_texture(&matl_info->texture);
 
-	if (material_info->type == MATERIAL_LAMBERTIAN)
+	if (matl_info->type == MATERIAL_LAMBERTIAN)
 		return (new_lambertian(texture));
-	else if (material_info->type == MATERIAL_METAL)
-		return (new_metal(texture, material_info->fuzz));
-	else if (material_info->type == MATERIAL_DIELECTRIC)
-		return (new_dielectric(material_info->refractive_idx));
-	else if (material_info->type == MATERIAL_EMMISIVE)
-		return (new_emmisive(material_info->texture.rgb1));
-	else if (material_info->type == MATERIAL_RANDOM)
+	else if (matl_info->type == MATERIAL_METAL)
+		return (new_metal(texture, matl_info->fuzz));
+	else if (matl_info->type == MATERIAL_DIELECTRIC)
+		return (new_dielectric(matl_info->refractive_idx));
+	else if (matl_info->type == MATERIAL_EMMISIVE)
+		return (new_emmisive(matl_info->texture.rgb1));
+	else if (matl_info->type == MATERIAL_RANDOM)
 		return (new_random_material());
 	return (NULL);
 }
