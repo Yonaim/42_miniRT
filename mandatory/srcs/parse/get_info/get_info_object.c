f3fc0cf42b141ed9205df71fbc66ce7314afc8b8
yonaCluster <yonaimyo@gmail.com>
2023-03-07 21:46:57 +0900

    Fix: get_info_texture offset값 조정

--- mandatory/srcs/parse/get_info/get_info_object.c
+++ mandatory/srcs/parse/get_info/get_info_object.c
@@ -21,6 +21,7 @@ t_info_texture get_info_texture(const t_token_arr *tokens, int offset)
 	t_info_texture	tex_info;
 
 	tex_info.type = get_texture_type(nth_token(tokens, offset)->str);
+	offset++;
 	if (tex_info.type == TEXTURE_SOLID)
 		tex_info.rgb1 = parse_vector3(tokens, &offset);
 	else if (tex_info.type == TEXTURE_CHECKER)
