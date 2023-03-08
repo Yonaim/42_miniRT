e70b875d9cfa9dc7d773b6bf8690b1b0b67e34f4
yonaCluster <yonaimyo@gmail.com>
2023-03-09 06:57:11 +0900

    Fix: material 파싱 random 유효성 검사

--- mandatory/srcs/parse/check_str/is_valid_material1.c
+++ mandatory/srcs/parse/check_str/is_valid_material1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_valid_material1.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yeonhkim  <yeonhkim@student.42seoul.>      +#+  +:+       +#+        */
+/*   By: yeonhkim <yeonhkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 22:03:19 by yeonhkim          #+#    #+#             */
-/*   Updated: 2023/03/09 03:42:44 by yeonhkim         ###   ########.fr       */
+/*   Updated: 2023/03/09 06:41:37 by yeonhkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ bool	is_valid_material_default(const t_token_arr *tokens, int offset)
 
 bool	is_valid_material_random(const t_token_arr *tokens, int offset)
 {
-	if (tokens->cnt == offset + 3 \
-		&& nth_token(tokens, offset + 1)->type == TOKEN_IDENTIFIER_MATERIAL
-		&& ft_strcmp(nth_token(tokens, offset + 1)->str, \
+	if (tokens->cnt == offset + 2 \
+		&& nth_token(tokens, ++offset)->type == TOKEN_IDENTIFIER_MATERIAL
+		&& ft_strcmp(nth_token(tokens, offset)->str, \
 											IDENTIFIER_MATERIAL_RANDOM) == 0)
 		return (true);
 	else
@@ -49,7 +49,6 @@ bool	is_valid_material(const t_token_arr *tokens, int offset)
 		is_valid_material_random,
 	};
 	int							i;
-
 	if (nth_token(tokens, offset)->type != TOKEN_PIPE)
 		return (false);
 	i = 0;
